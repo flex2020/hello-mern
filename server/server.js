3314c76ad1972900dae8b7305b39ba3ef6d16c3a
flex2020 <hosan_1@naver.com>
2022-11-07 22:55:19 +0900

    mongoDB 연결 및 api 호출 예제

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB(); 
+
+app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/user', require('./routes/api/user'));
+
+const PORT = process.env.PORT || 5000; // 사용할 포트 설정
+
+app.listen(PORT, () => console.log(`Server starts on port ${PORT}`));
