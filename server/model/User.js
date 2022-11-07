c387a03c29e92e018148b3a25b3e3bcf83edbc51
flex2020 <hosan_1@naver.com>
2022-11-07 23:38:46 +0900

    User의 스키마 정의

---
+++ server/model/User.js
@@ -0,0 +1,29 @@
+//--- DATA 관리
+
+const mongoose = require('mongoose');
+
+// User스키마 정의
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
